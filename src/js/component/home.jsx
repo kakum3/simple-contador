92ec1191286c8b2600f8dc02b4c5a960a9737df8
kakum3 <kakum_3@hotmail.com>
2022-08-04 08:26:23 +0000

    cambiado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,17 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const SimpleCounter = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="Reloj"><i className="fa fa-clock"></i></div>
+			<div className="Six">{props.digitSix % 10}</div>
+			<div className="Five">{props.digitFive % 10}</div>
+			<div className="Four">{props.digitFour % 10}</div>
+			<div className="Three">{props.digitThree % 10}</div>
+			<div className="Two">{props.digitTwo % 10}</div>
+			<div className="One">{props.digitOne % 10}</div>
 		</div>
 	);
 };
 
-export default Home;
+export default SimpleCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,20 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import SimpleCounter from "./component/home.jsx";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let Counter = 0;
+setInterval(function(){
+const Six = Math.floor(Counter/100000);
+const Five = Math.floor(Counter/10000);
+const Four = Math.floor(Counter/1000);
+const Three = Math.floor(Counter/100);
+const Two = Math.floor(Counter/10);
+const One = Math.floor(Counter/1);
+Counter++;
+
+
+
+ReactDOM.render(<SimpleCounter digitOne={One} digitTwo={Two} digitThree={Three} digitFour={Four} digitFive={Five} digitSix={Six}/>, document.querySelector("#app"));
+}, 1000);
